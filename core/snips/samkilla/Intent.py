57cbbd630f4c091928bb91bd28d047f98d713b6c
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-09-01 16:08:00 +0200

    re #5 Sanitized Intent.py

--- core/snips/samkilla/Intent.py
+++ core/snips/samkilla/Intent.py
@@ -2,59 +2,51 @@
 
 import hashlib
 import re
+import typing
 
+from core.snips import SamkillaManager
 from core.snips.samkilla.exceptions.HttpError import HttpError
 from core.snips.samkilla.exceptions.IntentError import IntentError
 from core.snips.samkilla.gql.intents.deleteIntent import deleteIntent
 from core.snips.samkilla.gql.intents.publishIntent import publishIntent
-from core.snips.samkilla.gql.intents.queries import fullIntentQuery
-from core.snips.samkilla.gql.intents.queries import intentsByUserIdWithUsageQuery
+from core.snips.samkilla.gql.intents.queries import fullIntentQuery, intentsByUserIdWithUsageQuery
 from core.snips.samkilla.gql.skills.patchSkillIntents import patchSkillIntents
 
 UTTERANCES_DEFINITION_REGEX = re.compile(r'{(.*?):=>(.*?)}')
 
-class Intent:
 
+class Intent:
 	GLUE_SLOT_WORD = ":=>"
 
-	def __init__(self, ctx):
+
+	def __init__(self, ctx: SamkillaManager):
 		self._ctx = ctx
 		self._cacheInit = False
 		self._intentsCache = {'cacheId': dict(), 'cacheName': dict()}
 
 
 	def getIntentByUserIdAndIntentName(self, userId: str, intentName: str) -> dict:
-		if intentName.lower() in self._intentsCache['cacheName']:
-			intent = self._intentsCache['cacheName'][intentName.lower()]
-		else:
-			intent = self.listIntentsByUserId(userId, intentFilter=intentName, intentFilterAttribute='name')
-
-		return intent
+		return self._intentsCache['cacheName'].get(intentName.lower(), self.listIntentsByUserId(userId, intentFilter=intentName, intentFilterAttribute='name'))
 
 
-	def getIntentByUserIdAndIntentId(self, userId, intentId):
-		if intentId in self._intentsCache['cacheId']:
-			intent = self._intentsCache['cacheId'][intentId]
-		else:
-			intent = self.listIntentsByUserId(userId, intentFilter=intentId)
+	def getIntentByUserIdAndIntentId(self, userId: str, intentId: str) -> dict:
+		return self._intentsCache['cacheId'].get(intentId, self.listIntentsByUserId(userId, intentFilter=intentId))
 
-		return intent
 
-	def listIntentsByUserId(self, userId, intentFilter=None, languageFilter=None, intentFilterAttribute='id', returnAllCacheIndexedBy=None):
-		variables = { 'userId': userId }
+	def listIntentsByUserId(self, userId: str, intentFilter: str = None, languageFilter: str = None, intentFilterAttribute: str = 'id', returnAllCacheIndexedBy: str = None) -> dict:
+		variables = {'userId': userId}
 
 		if languageFilter:
 			variables['lang'] = languageFilter
 
 		gqlRequest = [{
 			'operationName': 'IntentsByUserIdWithUsageQuery',
-			'variables': variables,
-			'query': intentsByUserIdWithUsageQuery
+			'variables'    : variables,
+			'query'        : intentsByUserIdWithUsageQuery
 		}]
 		response = self._ctx.postGQLBrowserly(gqlRequest)
 
 		self._cacheInit = True
-
 
 		for intent in response['intents']:
 			self._intentsCache['cacheId'][intent['id']] = intent
@@ -73,7 +65,7 @@ class Intent:
 		return response['intents']
 
 
-	def listIntentsByUserIdAndSkillId(self, userId, skillId, languageFilter=None, indexedBy=None, fromCache=False):
+	def listIntentsByUserIdAndSkillId(self, userId: str, skillId: str, languageFilter: str = None, indexedBy: str = None, fromCache: bool = False) -> typing.Iterable:
 		if fromCache and self._cacheInit:
 			intents = self._intentsCache['cacheId'].values()
 		else:
@@ -97,58 +89,60 @@ class Intent:
 		return skillIntents
 
 
-	def listUtterancesByIntentId(self, intentId):
-		variables = { 'intentId': intentId }
+	def listUtterancesByIntentId(self, intentId: str) -> list:
+		variables = {'intentId': intentId}
 
 		gqlRequest = [{
 			'operationName': 'FullIntentQuery',
-			'variables': variables,
-			'query': fullIntentQuery
+			'variables'    : variables,
+			'query'        : fullIntentQuery
 		}]
 		response = self._ctx.postGQLBrowserly(gqlRequest)
 
 		return response['intent']['customIntentData']['utterances']
 
 
+	def create(self, userId: str, language: str, skillId: str, name: str = 'Untitled', description: str = '', enabledByDefault: bool = True,
+	           attachToSkill: bool = True, typeEntityMatching: dict = None, slotsDefinition: dict = None, utterancesDefinition: list = None) -> str:
+		"""
+		Warning: mind the language parameter if the skill language is EN, intent must set language to EN
+		no error will be shown and the skill won't be created
+		"""
 
-	# Warning: mind the language parameter if the skill language is EN, intent must set language to EN
-	# no error will be shown and the skill won't be created
-	def create(self, userId, language, skillId, name='Untitled', description='', enabledByDefault=True, attachToSkill=True, typeEntityMatching=None, slotsDefinition=None, utterancesDefinition=None):
-
-		(structuredSlots, entities) = self.formatSlotsAndEntities(typeEntityMatching, slotsDefinition)
-		(structuredUtterances, exempleQueries) = self.formatUtterancesAndExempleQueries(utterancesDefinition)
+		structuredSlots, entities = self.formatSlotsAndEntities(typeEntityMatching, slotsDefinition)
+		structuredUtterances, exempleQueries = self.formatUtterancesAndExempleQueries(utterancesDefinition)
 
 		finalStructuredUtterances = list()
 
 		if structuredUtterances:
 			finalStructuredUtterances = structuredUtterances
 		else:
-			finalStructuredUtterances.append({ 'data': [{'range': { 'start': 0, 'end': 0 }, 'text': '' }] })
+			finalStructuredUtterances.append({'data': [{'range': {'start': 0, 'end': 0}, 'text': ''}]})
 
 		gqlRequest = [{
 			'operationName': 'publishIntent',
-			'variables': {
+			'variables'    : {
 				'input': {
-					'config': {
-						'author': self._ctx.userEmail,
-						'description': description,
-						'displayName': name,
+					'config' : {
+						'author'          : self._ctx.userEmail,
+						'description'     : description,
+						'displayName'     : name,
 						'enabledByDefault': enabledByDefault,
-						'exampleQueries': exempleQueries,
-						'language': language,
-						'name': name,
-						'private': True,
-						'slots': structuredSlots,
-						'version': '0'
+						'exampleQueries'  : exempleQueries,
+						'language'        : language,
+						'name'            : name,
+						'private'         : True,
+						'slots'           : structuredSlots,
+						'version'         : '0'
 					},
 					'dataset': {
-						'entities': entities,
-						'language': language,
+						'entities'  : entities,
+						'language'  : language,
 						'utterances': finalStructuredUtterances
 					}
 				}
 			},
-			'query': publishIntent
+			'query'        : publishIntent
 		}]
 
 		try:
@@ -163,7 +157,7 @@ class Intent:
 						for skillItem in intentDuplicate['usedIn']:
 							self.removeFromSkill(intentId=intentDuplicate['id'], skillId=skillItem['skillId'], userId=userId, deleteAfter=False)
 					self.delete(intentId=intentDuplicate['id'])
-					return self.create(userId,  language, skillId, name, description, enabledByDefault, attachToSkill, typeEntityMatching, slotsDefinition, utterancesDefinition)
+					return self.create(userId, language, skillId, name, description, enabledByDefault, attachToSkill, typeEntityMatching, slotsDefinition, utterancesDefinition)
 
 			raise he
 
@@ -174,7 +168,8 @@ class Intent:
 
 		return createdIntentId
 
-	def attachToSkill(self, userId, skillId, intentId, languageFilter=None):
+
+	def attachToSkill(self, userId: str, skillId: str, intentId: str, languageFilter: str = None):
 		existingIntents = self.listIntentsByUserIdAndSkillId(userId=userId, skillId=skillId, languageFilter=languageFilter)
 		variablesIntents = [{'id': intentId}]
 
@@ -183,59 +178,60 @@ class Intent:
 
 		gqlRequest = [{
 			'operationName': 'patchSkillIntents',
-			'variables': {
+			'variables'    : {
 				'input': {
-					'id': skillId,
+					'id'     : skillId,
 					'intents': variablesIntents
 				}
 			},
-			'query': patchSkillIntents
+			'query'        : patchSkillIntents
 		}]
 		self._ctx.postGQLBrowserly(gqlRequest, rawResponse=True)
 
 
-	def removeFromSkill(self, userId, skillId, intentId, languageFilter=None, deleteAfter=True):
+	def removeFromSkill(self, userId: str, skillId: str, intentId: str, languageFilter: str = None, deleteAfter: bool = True):
 		existingIntents = self.listIntentsByUserIdAndSkillId(userId=userId, skillId=skillId, languageFilter=languageFilter)
 
 		variablesIntents = list()
 
 		for existingIntent in existingIntents:
 			if existingIntent['id'] != intentId:
-				variablesIntents.append({ 'id': existingIntent['id'] })
+				variablesIntents.append({'id': existingIntent['id']})
 
 		gqlRequest = [{
 			'operationName': 'patchSkillIntents',
-			'variables': {
+			'variables'    : {
 				'input': {
-					'id': skillId,
+					'id'     : skillId,
 					'intents': variablesIntents
 				}
 			},
-			'query': patchSkillIntents
+			'query'        : patchSkillIntents
 		}]
 		self._ctx.postGQLBrowserly(gqlRequest, rawResponse=True)
 
 		if deleteAfter:
 			self.delete(intentId=intentId)
 
-	def delete(self, intentId):
+
+	def delete(self, intentId: str):
 		gqlRequest = [{
 			'operationName': 'deleteIntent',
-			'variables':  {'intentId': intentId},
-			'query': deleteIntent
+			'variables'    : {'intentId': intentId},
+			'query'        : deleteIntent
 		}]
 		response = self._ctx.postGQLBrowserly(gqlRequest, rawResponse=True)
 		return response
 
 
-	def edit(self, intentId, userId, language=None, skillId=None, name=None, description=None, enabledByDefault=True, attachToSkill=False,
-			 typeEntityMatching=None, slotsDefinition=None, utterancesDefinition=None):
-		(structuredSlots, entities) = self.formatSlotsAndEntities(typeEntityMatching, slotsDefinition)
-		(structuredUtterances, exempleQueries) = self.formatUtterancesAndExempleQueries(utterancesDefinition)
+	def edit(self, intentId: str, userId: str, language: str = None, skillId: str = None, name: str = None, description: str = None, enabledByDefault: bool = True,
+	         attachToSkill: bool = False, typeEntityMatching: dict = None, slotsDefinition: dict = None, utterancesDefinition: list = None):
+		structuredSlots, entities = self.formatSlotsAndEntities(typeEntityMatching, slotsDefinition)
+		structuredUtterances, exempleQueries = self.formatUtterancesAndExempleQueries(utterancesDefinition)
 
 		intent = self.getIntentByUserIdAndIntentId(userId=userId, intentId=intentId)
 
-		if intent is None:
+		if not intent:
 			raise IntentError(4003, "Intent {} doesn't exist".format(intentId), ['intent'])
 
 		if name: intent['name'] = name
@@ -244,36 +240,37 @@ class Intent:
 
 		gqlRequest = [{
 			'operationName': 'publishIntent',
-			'variables': {
+			'variables'    : {
 				'intentId': intentId,
-				'input': {
-					'config': {
-						'author': intent['author'],
-						'description': intent['description'],
-						'displayName': intent['displayName'],
+				'input'   : {
+					'config' : {
+						'author'          : intent['author'],
+						'description'     : intent['description'],
+						'displayName'     : intent['displayName'],
 						'enabledByDefault': intent['enabledByDefault'],
-						'language': intent['language'],
-						'exampleQueries': exempleQueries,
-						'slots': structuredSlots,
-						'version': intent['version'],
-						'name': intent['name'],
-						'private': True,
+						'language'        : intent['language'],
+						'exampleQueries'  : exempleQueries,
+						'slots'           : structuredSlots,
+						'version'         : intent['version'],
+						'name'            : intent['name'],
+						'private'         : True,
 					},
 					'dataset': {
-						"entities": entities,
-						"language": intent['language'],
+						"entities"  : entities,
+						"language"  : intent['language'],
 						"utterances": structuredUtterances
 					}
 				}
 			},
-			"query": publishIntent
+			"query"        : publishIntent
 		}]
 		self._ctx.postGQLBrowserly(gqlRequest, rawResponse=True)
 
 		if attachToSkill:
 			self.attachToSkill(userId=userId, skillId=skillId, intentId=intentId, languageFilter=language)
 
-	def formatSlotsAndEntities(self, typeEntityMatching, slotsDefinition):
+
+	def formatSlotsAndEntities(self, typeEntityMatching: dict, slotsDefinition: dict) -> tuple:
 		entities = list()
 		structuredSlots = list()
 
@@ -284,18 +281,19 @@ class Intent:
 			entities.append({"id": slotEntityId, "name": slotEntityId if snipsSpecialSlot else slot['type']})
 
 			structuredSlots.append({
-				"entityId": slotEntityId,
-				"id": self.hashSlotName(slotName=slot['name']),
+				"entityId"       : slotEntityId,
+				"id"             : self.hashSlotName(slotName=slot['name']),
 				"missingQuestion": slot['missingQuestion'],
-				"name": slot['name'],
-				"description": slot['description'],
-				"required": slot['required'],
-				"parameters": None
+				"name"           : slot['name'],
+				"description"    : slot['description'],
+				"required"       : slot['required'],
+				"parameters"     : None
 			})
 
 		return structuredSlots, entities
 
-	def formatUtterancesAndExempleQueries(self, utterances):
+
+	def formatUtterancesAndExempleQueries(self, utterances: list) -> tuple:
 		exempleQueries = list()
 		structuredUtterances = list()
 
@@ -336,27 +334,29 @@ class Intent:
 				if formattedWordSlotIndexStart != 0:
 					previousText = formattedTextUtterance[lastPieceIndex:formattedWordSlotIndexStart]
 					data.append(
-						{"text": previousText, "range": {"start": lastPieceIndex, "end": formattedWordSlotIndexStart}})
+						{'text': previousText, 'range': {'start': lastPieceIndex, 'end': formattedWordSlotIndexStart}}
+					)
 
 				lastPieceIndex = formattedWordSlotIndexEnd
 				sumIndexOffset += indexOffset
 				counterItems += 1
 
-				data.append({"slot_id": self.hashSlotName(slotName=wordSlotName), "slot_name": wordSlotName, "text": wordExemple,
-							 "range": {"start": formattedWordSlotIndexStart, "end": formattedWordSlotIndexEnd}})
+				data.append({'slot_id': self.hashSlotName(slotName=wordSlotName), 'slot_name': wordSlotName, 'text': wordExemple,
+				             'range'  : {'start': formattedWordSlotIndexStart, 'end': formattedWordSlotIndexEnd}})
 
 				if counterItems == maxItems and lastPieceIndex < lenCleanTextUtterance:
 					endText = formattedTextUtterance[lastPieceIndex:lenCleanTextUtterance]
-					data.append({"text": endText, "range": {"start": lastPieceIndex, "end": lenCleanTextUtterance}})
+					data.append({'text': endText, 'range': {'start': lastPieceIndex, 'end': lenCleanTextUtterance}})
 
 			if len(data) == 0:
-				data.append({"text": formattedTextUtterance, "range": {"start": 0, "end": len(formattedTextUtterance)}})
+				data.append({'text': formattedTextUtterance, 'range': {'start': 0, 'end': len(formattedTextUtterance)}})
 
-			structuredUtterances.append({"data": data})
+			structuredUtterances.append({'data': data})
 			exempleQueries.append(formattedTextUtterance)
 
 		return structuredUtterances, exempleQueries[0:10]
 
+
 	@staticmethod
-	def hashSlotName(slotName):
+	def hashSlotName(slotName: str) -> str:
 		return hashlib.sha512(slotName.encode('utf-8')).hexdigest()[0:9]
