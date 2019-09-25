2e3c94371e0ac3d612ea1bb8c942db0131962462
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-09-25 21:39:58 +0200

    Time widget

---
+++ modules/DateDayTimeYear/widgets/js/Clock.js
@@ -0,0 +1,26 @@
+(function () {
+    let up = true
+    function checkTime(i) {
+        return (i < 10) ? '0' + i : i;
+    }
+
+    function startTime() {
+        let today = new Date(),
+            h = checkTime(today.getHours()),
+            m = checkTime(today.getMinutes());
+
+        if (up) {
+            $('#DateDayTimeYear_clock').html(h + ':' + m);
+        }
+        else {
+            $('#DateDayTimeYear_clock').html(h + ' ' + m);
+        }
+
+        up = !up;
+
+        setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+    startTime();
+})();
