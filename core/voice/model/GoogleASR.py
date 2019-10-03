86caff31cfa0c829e185e0b3a5929819963943a6
Project Alice <alice@projectalice.ch>
2019-10-03 22:58:33 +0200

    fixed google asr import and bad path type (from Path to str(Path))

--- core/voice/model/GoogleASR.py
+++ core/voice/model/GoogleASR.py
@@ -15,7 +15,7 @@ class GoogleASR(ASR):
 	def __init__(self):
 		super().__init__()
 
-		os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = Path(commons.rootDir(), 'credentials/googlecredentials.json')
+		os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str(Path(commons.rootDir(), 'credentials/googlecredentials.json'))
 
 		self._client = speech.SpeechClient()
 		self._config = types.RecognitionConfig(
