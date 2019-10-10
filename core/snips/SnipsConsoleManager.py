d1b2433dfb0f8454958bd262517e9036c6c682e5
maxbachmann <kontakt@maxbachmann.de>
2019-10-10 13:08:29 +0200

    cleanup kwargs stuff

--- core/snips/SnipsConsoleManager.py
+++ core/snips/SnipsConsoleManager.py
@@ -56,8 +56,7 @@ class SnipsConsoleManager(Manager):
 
 	def doDownload(self, modulesInfos: dict = None):
 		self._logger.info(f'[{self.name}] Starting Snips assistant training and download procedure')
-		kwargs = {'moduleInfos': modulesInfos}
-		self.ThreadManager.newEvent('SnipsAssistantDownload', onClearCallback='onSnipsAssistantDownloaded').set(**kwargs)
+		self.ThreadManager.newEvent('SnipsAssistantDownload', onClearCallback='onSnipsAssistantDownloaded').set(moduleInfos=modulesInfos)
 
 		projectId = self.LanguageManager.activeSnipsProjectId
 		self.ThreadManager.newThread(name='SnipsAssistantDownload', target=self.download, args=[projectId])
