c4ef2016aa8f252de3a06d8f985c77b2789083fb
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-10-17 15:12:19 +0200

    Fixing hardcoded pi user

--- system/scripts/audioHardware/respeakerCoreV2.sh
+++ system/scripts/audioHardware/respeakerCoreV2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo -u pi bash <<EOF
+sudo -u "$(logname)" bash <<EOF
     /home/pi/snipsLedControl/venv/bin/pip3 uninstall -y gpiozero
     /home/pi/snipsLedControl/venv/bin/pip3 uninstall -y RPi.GPIO
 EOF
