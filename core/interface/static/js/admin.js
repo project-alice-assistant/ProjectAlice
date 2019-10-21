56ea466aad9a2a94f698737070c18e6d69ce9293
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-10-21 18:52:35 +0200

    Slowly getting there, interface alice configs

--- core/interface/static/js/admin.js
+++ core/interface/static/js/admin.js
@@ -1,3 +1,34 @@
 $(function () {
-    $('#adminPageTabs').tabs();
+
+	let $defaultTab = $('#adminPageTabsContainer ul li:first');
+	$defaultTab.addClass('activeTab');
+
+	$('.tabsContent').children().each(function () {
+		if ($(this).attr('id') == $defaultTab.data('for')) {
+			$(this).show();
+		}
+		else {
+			$(this).hide();
+		}
+	});
+
+	$('.tab').on('click', function () {
+		let target = $(this).data('for');
+		$(this).addClass('activeTab');
+
+		$('#adminPageTabsContainer ul li').each(function () {
+			if ($(this).data('for') != target) {
+				$(this).removeClass('activeTab');
+			}
+		});
+
+		$('.tabsContent').children().each(function () {
+			if ($(this).attr('id') == target) {
+				$(this).show();
+			}
+			else {
+				$(this).hide();
+			}
+		});
+	})
 });
