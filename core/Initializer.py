a02994b1ec716311b5f67ffdc5a375b0e720f18d
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-10-29 09:40:27 +0100

    Adding pip uninstall in initializer

--- core/Initializer.py
+++ core/Initializer.py
@@ -137,6 +137,7 @@ network={
 
 		# Do some installation if wanted by the user
 		if initConfs['doGroundInstall']:
+			subprocess.run(['./venv/bin/pip3', 'uninstall', '-r', str(Path(self._rootDir, 'pipuninstalls.txt'))])
 			subprocess.run(['./venv/bin/pip3', 'install', '-r', str(Path(self._rootDir, 'piprequirements.txt'))])
 
 			subprocess.run(['sudo', 'bash', '-c', 'echo "deb https://raspbian.snips.ai/$(lsb_release -cs) stable main" > /etc/apt/sources.list.d/snips.list'])
