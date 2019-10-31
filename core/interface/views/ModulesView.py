298ccfbacc561acb043af15f0548f758b9fcfc9f
maxbachmann <kontakt@maxbachmann.de>
2019-10-31 21:57:55 +0100

    reduce complexity/runtime to build module dict

--- core/interface/views/ModulesView.py
+++ core/interface/views/ModulesView.py
@@ -10,10 +10,8 @@ class ModulesView(View):
 
 
 	def index(self):
-		modules = {moduleName: module['instance'] for moduleName, module in self.ModuleManager.getModules(False).items()}
-		deactivatedModules = {moduleName: module['instance'] for moduleName, module in self.ModuleManager.deactivatedModules.items()}
-		modules = {**modules, **deactivatedModules}
-		modules = dict(sorted(modules.items()))
+		modules = {**self.ModuleManager.getModules(False), **self.ModuleManager.deactivatedModules}
+		modules = {moduleName: module['instance'] for moduleName, module in sorted(modules.items())}
 
 		return render_template(template_name_or_list='modules.html',
 		                       modules=modules,
@@ -24,7 +22,7 @@ class ModulesView(View):
 
 	def toggleModule(self):
 		try:
-			action, module = request.form.get('id').split('_')
+			_, module = request.form.get('id').split('_')
 			if self.ModuleManager.isModuleActive(module):
 				self.ModuleManager.deactivateModule(moduleName=module, persistent=True)
 			else:
@@ -38,7 +36,7 @@ class ModulesView(View):
 
 	def deleteModule(self):
 		try:
-			action, module = request.form.get('id').split('_')
+			_, module = request.form.get('id').split('_')
 			self.ModuleManager.removeModule(module)
 			return self.index()
 		except Exception as e:
