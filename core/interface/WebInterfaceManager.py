7cbf3e384dc086bc5d25c0a1b0c78de5782880c4
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-11-14 15:43:14 +0100

    Might explain why favicon missing

--- core/interface/WebInterfaceManager.py
+++ core/interface/WebInterfaceManager.py
@@ -36,9 +36,11 @@ class WebInterfaceManager(Manager):
 		self._langData = dict()
 		self._moduleInstallProcesses = dict()
 		self._flaskLoginManager = None
-		
+
+
+	# noinspection PyMethodParameters
 	@app.route('/favicon.ico')
-	def favicon(self):
+	def favicon():
 		return send_from_directory('static/','favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 	@app.route('/base/<path:filename>')
