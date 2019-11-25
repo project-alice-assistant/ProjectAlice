42d404348aa5196afe28c4f1518b598680bdbea2
maxbachmann <ssh@maxbachmann.de>
2019-11-25 08:57:40 +0100

    add info log to see which module consumed an intent

--- core/server/MqttManager.py
+++ core/server/MqttManager.py
@@ -209,6 +209,7 @@ class MqttManager(Manager):
 					return
 
 				elif consumed or consumed is None:
+					self.logInfo(f'The intent {message.topic} was consumed by {modul.__class__.__name__}')
 					return
 
 			self.logWarning(f"Intent \"{message.topic}\" wasn't consumed by any module")
