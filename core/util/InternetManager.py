020a85f8488cb4c29b1b39716a306fd8e069e2ca
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-12-13 17:33:17 +0100

    Moving internet lost and back to events in alice core skill

--- core/util/InternetManager.py
+++ core/util/InternetManager.py
@@ -16,7 +16,7 @@ class InternetManager(Manager):
 	def onStart(self):
 		super().onStart()
 		if not self.ConfigManager.getAliceConfigByName('stayCompletlyOffline'):
-			self.checkOnlineState()
+			self.checkOnlineState(silent=True)
 		else:
 			self.logInfo('Configurations set to stay completly offline')
 
@@ -35,12 +35,15 @@ class InternetManager(Manager):
 			self.checkOnlineState()
 
 
-	def checkOnlineState(self, addr: str = 'http://clients3.google.com/generate_204') -> bool:
+	def checkOnlineState(self, addr: str = 'http://clients3.google.com/generate_204', silent: bool = False) -> bool:
 		try:
 			online = requests.get(addr).status_code == 204
 		except RequestException:
 			online = False
-		
+
+		if silent:
+			return online
+
 		if self._online and not online:
 			self._online = False
 			self.broadcast(method='onInternetLost', exceptions=[self.name], propagateToSkills=True)
