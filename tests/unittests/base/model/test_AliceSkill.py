6e61762bfb2601afbe13ada53bc674e364798a97
maxbachmann <kontakt@maxbachmann.de>
2019-12-16 01:29:29 +0100

    add some more unittests

--- tests/unittests/base/model/test_AliceSkill.py
+++ tests/unittests/base/model/test_AliceSkill.py
@@ -47,7 +47,7 @@ class TestAliceSkill(unittest.TestCase):
 
 		self.assertFalse(intent1.protected)
 		self.assertEqual(intent1.authOnly, AccessLevel.ADMIN)
-		self.assertEqual(intent1.fallbackFunction, ExampleSkill.single_decorator)
+		self.assertEqual(intent1.fallbackFunction, exampleSkill.single_decorator)
 		self.assertEqual(intent1.dialogMapping, dict())
 		self.assertEqual(str(intent1), 'hermes/intent/unittest:intent1')
 
@@ -56,16 +56,104 @@ class TestAliceSkill(unittest.TestCase):
 		self.assertEqual(intent2.fallbackFunction, None)
 		self.assertDictEqual(
 			intent2.dialogMapping,
-			{'exampleState': ExampleSkill.multiple_decorator,
-			 'exampleState2': ExampleSkill.mqtt_decorator})
+			{'exampleState': exampleSkill.multiple_decorator,
+			 'exampleState2': exampleSkill.mqtt_decorator})
 		self.assertEqual(str(intent2), 'hermes/intent/unittest:intent2')
 
 		self.assertTrue(intent3.protected)
 		self.assertEqual(intent3.authOnly, 0)
-		self.assertEqual(intent3.fallbackFunction, ExampleSkill.multiple_decorator)
+		self.assertEqual(intent3.fallbackFunction, exampleSkill.multiple_decorator)
 		self.assertEqual(intent3.dialogMapping, dict())
 		self.assertEqual(str(intent3), 'hermes/intent/unittest:intent3')
 
+
+	@mock.patch('core.util.Decorators.Intent.ProtectedIntentManager', new_callable=PropertyMock)
+	@mock.patch('core.util.Decorators.Intent.ConfigManager', new_callable=PropertyMock)
+	@mock.patch('core.base.model.AliceSkill.AliceSkill.findDecoratedIntents')
+	def testBuildIntentList(self, mock_decoratedFuncs, mock_config, mock_protected):
+		owner_mock = MagicMock()
+		owner_mock.getAliceConfigByName.return_value = 'unittest'
+		mock_config.return_value = owner_mock
+
+		property_mock = MagicMock()
+		mock_protected.return_value = property_mock
+
+		class ExampleSkill(AliceSkill):
+			#ignore all stuff that would happen in the AliceSkill init
+			def __init__(self):
+				pass
+			
+			def exampleFunc(self):
+				pass
+
+		exampleSkill = ExampleSkill()
+
+
+		# no decorated functions no initialised values
+		mock_decoratedFuncs.return_value = {}
+		self.assertDictEqual(
+			exampleSkill.buildIntentList(list()),
+			dict()
+		)
+
+		# no decorated functions
+		initIntents = [
+			Intent('intent1', authOnly=AccessLevel.ADMIN),
+			'hermes/intent/unittest:intent2',
+			(Intent('intent3'), exampleSkill.exampleFunc),
+			('hermes/intent/unittest:intent4', exampleSkill.exampleFunc)
+		]
+
+		mappings = exampleSkill.buildIntentList(initIntents)
+		intent1 =  mappings[str(Intent('intent1'))]
+		intent2 =  mappings[str(Intent('intent2'))]
+		intent3 =  mappings[str(Intent('intent3'))]
+		intent4 =  mappings[str(Intent('intent4'))]
+
+		self.assertFalse(intent1.protected)
+		self.assertEqual(intent1.authOnly, AccessLevel.ADMIN)
+		self.assertEqual(intent1.fallbackFunction, None)
+		self.assertEqual(intent1.dialogMapping, dict())
+		self.assertEqual(str(intent1), 'hermes/intent/unittest:intent1')
+
+		self.assertFalse(intent2.protected)
+		self.assertEqual(intent2.authOnly, 0)
+		self.assertEqual(intent2.fallbackFunction, None)
+		self.assertEqual(intent2.dialogMapping, dict())
+		self.assertEqual(str(intent2), 'hermes/intent/unittest:intent2')
+
+		self.assertFalse(intent3.protected)
+		self.assertEqual(intent3.authOnly, 0)
+		self.assertEqual(intent3.fallbackFunction, exampleSkill.exampleFunc)
+		self.assertEqual(intent3.dialogMapping, dict())
+		self.assertEqual(str(intent3), 'hermes/intent/unittest:intent3')
+
+		self.assertFalse(intent4.protected)
+		self.assertEqual(intent4.authOnly, 0)
+		self.assertEqual(intent4.fallbackFunction, exampleSkill.exampleFunc)
+		self.assertEqual(intent4.dialogMapping, dict())
+		self.assertEqual(str(intent4), 'hermes/intent/unittest:intent4')
+
+
+		# decorated functions
+		intent1 = Intent('intent1')
+		intent1.fallbackFunction = exampleSkill.onMessage
+		mock_decoratedFuncs.return_value = {
+			'hermes/intent/unittest:intent1': intent1,
+		}
+
+		initIntents = [
+			Intent('intent1', authOnly=AccessLevel.ADMIN)
+		]
+
+		mappings = exampleSkill.buildIntentList(initIntents)
+		intent1 =  mappings[str(Intent('intent1'))]
+
+		self.assertFalse(intent1.protected)
+		self.assertEqual(intent1.authOnly, AccessLevel.ADMIN)
+		self.assertEqual(intent1.fallbackFunction, exampleSkill.onMessage)
+		self.assertEqual(intent1.dialogMapping, dict())
+		self.assertEqual(str(intent1), 'hermes/intent/unittest:intent1')
 
 
 if __name__ == "__main__":
