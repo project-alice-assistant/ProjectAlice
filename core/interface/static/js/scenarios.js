daeb9e8df08aa0b7163838a2a72f3da041145123
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-12-23 20:32:43 +0100

    Typo and input / outpu for scenarios

--- core/interface/static/js/scenarios.js
+++ core/interface/static/js/scenarios.js
@@ -8,13 +8,21 @@ $(function () {
 	);
 
 	$('dd').on('click touchstart', function() {
-		let $newDiv = $('<div class="scenariosTile">' + $(this).html()+ '</div>');
+		let $newDiv = $('<div class="scenariosTile">' + $(this).html() + '</div>');
 		$newDiv.draggable({
 				containment: '#program',
 				snap: '.scenariosTile',
 				grid: [10, 10]
 			}
 		);
+		let $output = $('<div class="scenarioOutputConnector">•</div>');
+		$newDiv.append($output);
+
+		if ($(this).data('type') != 'events') {
+			let $input = $('<div class="scenarioInputConnector">•</div>');
+			$newDiv.append($input);
+		}
+
 		$('#program').append($newDiv);
 	});
 
