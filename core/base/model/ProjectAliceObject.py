024e4e031ed63c4b33e71f3ad14c74693ac1440f
maxbachmann <ssh@maxbachmann.de>
2020-01-22 23:24:47 +0100

    use event funcs for type checking only

--- core/base/model/ProjectAliceObject.py
+++ core/base/model/ProjectAliceObject.py
@@ -1,3 +1,4 @@
+from typing import TYPE_CHECKING
 import json
 
 import core.base.SuperManager as SM
@@ -61,79 +62,6 @@ class ProjectAliceObject(Logger):
 
 		for name in deadManagers:
 			del SM.SuperManager.getInstance().managers[name]
-
-
-	def onStart(self): pass
-	def onStop(self): pass
-	def onBooted(self): pass
-	def onSkillInstalled(self): pass
-	def onSkillUpdated(self): pass
-	def onInternetConnected(self): pass
-	def onInternetLost(self): pass
-	def onHotword(self, siteId: str, user: str = constants.UNKNOWN_USER): pass
-	def onHotwordToggleOn(self, siteId: str): pass
-	def onSessionStarted(self, session): pass
-	def onStartListening(self, session): pass
-	def onCaptured(self, session): pass
-	def onIntentParsed(self, session): pass
-	def onUserCancel(self, session): pass
-	def onSessionTimeout(self, session): pass
-	def onIntentNotRecognized(self, session): pass
-	def onSessionError(self, session): pass
-	def onSessionEnded(self, session): pass
-	def onSay(self, session): pass
-	def onSayFinished(self, session): pass
-	def onSessionQueued(self, session): pass
-
-	def onMessage(self, session) -> bool:
-		""" Do not consume the intent by default """
-		return False
-
-	def onSleep(self): pass
-	def onWakeup(self): pass
-	def onGoingBed(self): pass
-	def onLeavingHome(self): pass
-	def onReturningHome(self): pass
-	def onEating(self): pass
-	def onWatchingTV(self): pass
-	def onCooking(self): pass
-	def onMakeup(self): pass
-	def onContextSensitiveDelete(self, sessionId: str): pass
-	def onContextSensitiveEdit(self, sessionId: str): pass
-	def onFullMinute(self): pass
-	def onFiveMinute(self): pass
-	def onQuarterHour(self): pass
-	def onFullHour(self): pass
-	def onWakeword(self, siteId: str, user: str = constants.UNKNOWN_USER): pass
-	def onMotionDetected(self): pass
-	def onMotionStopped(self): pass
-	def onButtonPressed(self): pass
-	def onButtonReleased(self): pass
-	def onDeviceConnecting(self): pass
-	def onDeviceDisconnecting(self): pass
-	def onUVIndexAlert(self, deviceList: list): pass
-	def onRaining(self, deviceList: list): pass
-	def onTooMuchRain(self, deviceList: list): pass
-	def onWindy(self, deviceList: list): pass
-	def onFreezing(self, deviceList: list): pass
-	def onTemperatureHighAlert(self, deviceList: list): pass
-	def onTemperatureLowAlert(self, deviceList: list): pass
-	def onCO2Alert(self, deviceList: list): pass
-	def onHumidityHighAlert(self, deviceList: list): pass
-	def onHumidityLowAlert(self, deviceList: list): pass
-	def onNoiseAlert(self, deviceList: list): pass
-	def onPressureHighAlert(self, deviceList: list): pass
-	def onPressureLowAlert(self, deviceList: list): pass
-	def onBroadcastingForNewDeviceStart(self, session): pass
-	def onBroadcastingForNewDeviceStop(self): pass
-	def onSnipsAssistantDownloaded(self, **kwargs): pass
-	def onSnipsAssistantDownloadFailed(self, **kwargs): pass
-	def onAuthenticated(self, session): pass
-	def onAuthenticationFailed(self, session): pass
-	def onAudioFrame(self, message): pass
-	def onSnipsAssistantInstalled(self, **kwargs): pass
-	def onSnipsAssistantFailedInstalling(self, **kwargs): pass
-	def onSkillInstallFailed(self, **kwargs): pass
 
 
 	@property
@@ -264,3 +192,71 @@ class ProjectAliceObject(Logger):
 	@property
 	def SkillStoreManager(self):
 		return SM.SuperManager.getInstance().skillStoreManager
+
+	if TYPE_CHECKING:
+		def onStart(self): pass
+		def onStop(self): pass
+		def onBooted(self): pass
+		def onSkillInstalled(self): pass
+		def onSkillUpdated(self): pass
+		def onInternetConnected(self): pass
+		def onInternetLost(self): pass
+		def onHotword(self, siteId: str, user: str = constants.UNKNOWN_USER): pass
+		def onHotwordToggleOn(self, siteId: str): pass
+		def onSessionStarted(self, session): pass
+		def onStartListening(self, session): pass
+		def onCaptured(self, session): pass
+		def onIntentParsed(self, session): pass
+		def onUserCancel(self, session): pass
+		def onSessionTimeout(self, session): pass
+		def onIntentNotRecognized(self, session): pass
+		def onSessionError(self, session): pass
+		def onSessionEnded(self, session): pass
+		def onSay(self, session): pass
+		def onSayFinished(self, session): pass
+		def onSessionQueued(self, session): pass
+		def onSleep(self): pass
+		def onWakeup(self): pass
+		def onGoingBed(self): pass
+		def onLeavingHome(self): pass
+		def onReturningHome(self): pass
+		def onEating(self): pass
+		def onWatchingTV(self): pass
+		def onCooking(self): pass
+		def onMakeup(self): pass
+		def onContextSensitiveDelete(self, sessionId: str): pass
+		def onContextSensitiveEdit(self, sessionId: str): pass
+		def onFullMinute(self): pass
+		def onFiveMinute(self): pass
+		def onQuarterHour(self): pass
+		def onFullHour(self): pass
+		def onWakeword(self, siteId: str, user: str = constants.UNKNOWN_USER): pass
+		def onMotionDetected(self): pass
+		def onMotionStopped(self): pass
+		def onButtonPressed(self): pass
+		def onButtonReleased(self): pass
+		def onDeviceConnecting(self): pass
+		def onDeviceDisconnecting(self): pass
+		def onUVIndexAlert(self, deviceList: list): pass
+		def onRaining(self, deviceList: list): pass
+		def onTooMuchRain(self, deviceList: list): pass
+		def onWindy(self, deviceList: list): pass
+		def onFreezing(self, deviceList: list): pass
+		def onTemperatureHighAlert(self, deviceList: list): pass
+		def onTemperatureLowAlert(self, deviceList: list): pass
+		def onCO2Alert(self, deviceList: list): pass
+		def onHumidityHighAlert(self, deviceList: list): pass
+		def onHumidityLowAlert(self, deviceList: list): pass
+		def onNoiseAlert(self, deviceList: list): pass
+		def onPressureHighAlert(self, deviceList: list): pass
+		def onPressureLowAlert(self, deviceList: list): pass
+		def onBroadcastingForNewDeviceStart(self, session): pass
+		def onBroadcastingForNewDeviceStop(self): pass
+		def onSnipsAssistantDownloaded(self, **kwargs): pass
+		def onSnipsAssistantDownloadFailed(self, **kwargs): pass
+		def onAuthenticated(self, session): pass
+		def onAuthenticationFailed(self, session): pass
+		def onAudioFrame(self, message): pass
+		def onSnipsAssistantInstalled(self, **kwargs): pass
+		def onSnipsAssistantFailedInstalling(self, **kwargs): pass
+		def onSkillInstallFailed(self, **kwargs): pass
