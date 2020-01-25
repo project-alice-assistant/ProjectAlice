58bd241df7d69bdb5f83df488366c1aa885de6f2
Psycho <laurentchervet@bluewin.ch>
2020-01-25 15:30:11 +0100

    DialogState object

---
+++ core/dialog/model/DialogState.py
@@ -0,0 +1,23 @@
+from core.base.SuperManager import SuperManager
+
+
+class DialogState:
+
+	def __init__(self, state: str):
+		if ':' not in state:
+			caller = SuperManager.getInstance().commons.getFunctionCaller(depth=2)
+			state = f'{caller}:{state}'
+
+		self._state = state
+
+
+	def __eq__(self, other) -> bool:
+		if ':' not in other:
+			caller = SuperManager.getInstance().commons.getFunctionCaller()
+			other = f'{caller}:{other}'
+
+		return self._state == other
+
+
+	def __ne__(self, other) -> bool:
+		return not self.__eq__(other)
