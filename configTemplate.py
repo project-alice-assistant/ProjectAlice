52eeb04b85e07c29a16c9f3309cc9605af480fbc
Psycho <laurentchervet@bluewin.ch>
2020-03-08 06:20:22 +0100

    Fix for #212 AWS regions missing

--- configTemplate.py
+++ configTemplate.py
@@ -1,216 +1,235 @@
 settings = {
-	'adminPinCode': {
+	'adminPinCode'          : {
 		'defaultValue': '',
 		'dataType'    : 'password',
 		'description' : 'Admin pin code, only numbers, 4 digits',
 		'beforeUpdate': 'checkNewAdminPinCode',
 		'onUpdate'    : 'updateAdminPinCode'
 	},
-	'autoReportSkillErrors': {
+	'autoReportSkillErrors' : {
 		'defaultValue': False,
-		'dataType': 'boolean',
-		'description': 'If true, an error thrown by a skill will automatically post a github issue and ping the author'
+		'dataType'    : 'boolean',
+		'description' : 'If true, an error thrown by a skill will automatically post a github issue and ping the author'
 	},
-	'notUnderstoodRetries' : {
+	'notUnderstoodRetries'  : {
 		'defaultValue': 3,
 		'dataType'    : 'integer',
 		'description' : 'Defines how many times Alice will ask to repeat if not understood before she gives up'
 	},
-	'ssid'                 : {
+	'ssid'                  : {
 		'defaultValue': '',
 		'dataType'    : 'string',
 		'description' : 'Your Wifi name'
 	},
-	'debug'                : {
+	'debug'                 : {
 		'defaultValue': False,
 		'dataType'    : 'boolean',
 		'description' : 'If true debug logs will show'
 	},
-	'wifipassword'         : {
+	'wifipassword'          : {
 		'defaultValue': '',
 		'dataType'    : 'password',
 		'description' : 'Your Wifi password'
 	},
-	'mqttHost'             : {
+	'mqttHost'              : {
 		'defaultValue': 'localhost',
 		'dataType'    : 'string',
 		'description' : 'Mqtt server ip adress',
 		'onUpdate'    : 'updateMqttSettings'
 	},
-	'mqttPort'             : {
+	'mqttPort'              : {
 		'defaultValue': 1883,
 		'dataType'    : 'integer',
 		'description' : 'Mqtt server port',
 		'onUpdate'    : 'updateMqttSettings'
 	},
-	'mqttUser'             : {
+	'mqttUser'              : {
 		'defaultValue': '',
 		'dataType'    : 'string',
 		'description' : 'Mqtt user. Leave blank if not password protected',
 		'onUpdate'    : 'updateMqttSettings'
 	},
-	'mqttPassword': {
+	'mqttPassword'          : {
 		'defaultValue': '',
 		'dataType'    : 'password',
 		'description' : 'Mqtt password. Leave blank if not password protected',
 		'onUpdate'    : 'updateMqttSettings'
 	},
-	'mqttTLSFile': {
+	'mqttTLSFile'           : {
 		'defaultValue': '',
 		'dataType'    : 'string',
 		'description' : 'Mqtt TLS file path for SSL',
 		'onUpdate'    : 'updateMqttSettings'
 	},
-	'micSampleRate': {
+	'micSampleRate'         : {
 		'defaultValue': 44100,
-		'dataType': 'integer',
-		'description': 'Your microphone sample rate'
+		'dataType'    : 'integer',
+		'description' : 'Your microphone sample rate'
 	},
-	'micChannels': {
+	'micChannels'           : {
 		'defaultValue': 1,
-		'dataType': 'integer',
-		'description': 'How many channel does your microphone support'
+		'dataType'    : 'integer',
+		'description' : 'How many channel does your microphone support'
 	},
-	'enableDataStoring': {
+	'enableDataStoring'     : {
 		'defaultValue': False,
-		'dataType': 'boolean',
-		'description': 'Enables local telemetry data storing'
+		'dataType'    : 'boolean',
+		'description' : 'Enables local telemetry data storing'
 	},
-	'autoPruneStoredData': {
+	'autoPruneStoredData'   : {
 		'defaultValue': 0,
-		'dataType': 'integer',
-		'description': 'Set to max entries to keep, 0 to disable pruning'
+		'dataType'    : 'integer',
+		'description' : 'Set to max entries to keep, 0 to disable pruning'
 	},
-	'probabilityThreshold': {
+	'probabilityThreshold'  : {
 		'defaultValue': 0.45,
-		'dataType': 'float',
-		'description': 'Capture intents with lower probability score than this settings will trigger Alice not understood'
+		'dataType'    : 'float',
+		'description' : 'Capture intents with lower probability score than this settings will trigger Alice not understood'
 	},
-	'stayCompletlyOffline': {
+	'stayCompletlyOffline'  : {
 		'defaultValue': False,
-		'dataType': 'boolean',
-		'description': 'Nothing goes out! Well, that also means no skill updates, no access to web APIs'
+		'dataType'    : 'boolean',
+		'description' : 'Nothing goes out! Well, that also means no skill updates, no access to web APIs'
 	},
-	'keepASROffline': {
+	'keepASROffline'        : {
 		'defaultValue': True,
-		'dataType': 'boolean',
-		'description': 'Do not use any online ASR such as Google ASR'
+		'dataType'    : 'boolean',
+		'description' : 'Do not use any online ASR such as Google ASR'
 	},
-	'keepTTSOffline': {
+	'keepTTSOffline'        : {
 		'defaultValue': True,
-		'dataType': 'boolean',
-		'description': 'Do not use any online TTS such as Google Wavenet or Amazon Polly'
+		'dataType'    : 'boolean',
+		'description' : 'Do not use any online TTS such as Google Wavenet or Amazon Polly'
 	},
-	'shortReplies': {
+	'shortReplies'          : {
 		'defaultValue': False,
-		'dataType': 'boolean',
-		'description': 'Use only short replies from Alice, when available'
+		'dataType'    : 'boolean',
+		'description' : 'Use only short replies from Alice, when available'
 	},
-	'whisperWhenSleeping': {
+	'whisperWhenSleeping'   : {
 		'defaultValue': True,
-		'dataType': 'boolean',
-		'description': 'Only available with Amazon Polly'
+		'dataType'    : 'boolean',
+		'description' : 'Only available with Amazon Polly'
 	},
 	'newDeviceBroadcastPort': {
 		'defaultValue': 12354,
-		'dataType': 'integer',
-		'description': 'Should be left as default, this is the port used to find new devices'
+		'dataType'    : 'integer',
+		'description' : 'Should be left as default, this is the port used to find new devices'
 	},
-	'asr': {
+	'asr'                   : {
 		'defaultValue': 'deepspeech',
 		'dataType'    : 'list',
 		'values'      : ['deepspeech', 'pocketsphinx', 'google'],
 		'description' : 'The ASR to use. Can\'t use an online ASR if you have set keepASROffline to true!',
 		'onUpdate'    : 'reloadASR'
 	},
-	'tts': {
+	'tts'                   : {
 		'defaultValue': 'pico',
-		'dataType': 'list',
-		'values': {'Pico': 'pico', 'Mycroft': 'mycroft', 'Amazon Polly': 'amazon', 'Google Wavenet': 'google', 'Snips Makers TTS': 'snips'},
-		'description': 'The TTS to use. Can\'t use an online TTS if you have set keepTTSOffline!'
+		'dataType'    : 'list',
+		'values'      : {'Pico': 'pico', 'Mycroft': 'mycroft', 'Amazon Polly': 'amazon', 'Google Wavenet': 'google', 'Snips Makers TTS': 'snips'},
+		'description' : 'The TTS to use. Can\'t use an online TTS if you have set keepTTSOffline!'
 	},
-	'ttsLanguage': {
+	'ttsLanguage'           : {
 		'defaultValue': 'en-US',
-		'dataType': 'string',
-		'description': 'Language for the TTS to use'
+		'dataType'    : 'string',
+		'description' : 'Language for the TTS to use'
 	},
-	'ttsType': {
+	'ttsType'               : {
 		'defaultValue': 'male',
-		'dataType': 'list',
-		'values': ['male', 'female'],
-		'description': 'Choose the voice gender you want'
+		'dataType'    : 'list',
+		'values'      : ['male', 'female'],
+		'description' : 'Choose the voice gender you want'
 	},
-	'ttsVoice'             : {
+	'ttsVoice'              : {
 		'defaultValue': 'en-US',
-		'dataType': 'string',
-		'description': 'The voice the TTS should use. Find lists on respective TTS websites'
+		'dataType'    : 'string',
+		'description' : 'The voice the TTS should use. Find lists on respective TTS websites'
 	},
-	'awsRegion'            : {
+	'awsRegion'             : {
 		'defaultValue': 'eu-central-1',
-		'dataType': 'list',
-		'values': ['eu-central-1', 'eu-west-1', ' eu-west-2', 'eu-west-3', 'eu-north-1', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'],
-		'description': 'Region to use for Amazon Polly'
+		'dataType'    : 'list',
+		'values'      : [
+			'eu-north-1',
+			'ap-south-1',
+			'eu-west-3',
+			'eu-west-2',
+			'eu-west-1',
+			'ap-northeast-2',
+			'me-south-1',
+			'ap-northeast-1',
+			'sa-east-1',
+			'ca-central-1',
+			'ap-east-1',
+			'ap-southeast-1',
+			'ap-southeast-2',
+			'eu-central-1',
+			'us-east-1',
+			'us-east-2',
+			'us-west-1',
+			'us-west-2'
+		],
+		'description' : 'Region to use for Amazon Polly'
 	},
-	'awsAccessKey'         : {
+	'awsAccessKey'          : {
 		'defaultValue': '',
 		'dataType'    : 'password',
 		'description' : 'Your Amazon services access key'
 	},
-	'awsSecretKey'         : {
+	'awsSecretKey'          : {
 		'defaultValue': '',
 		'dataType'    : 'password',
 		'description' : 'Your Amazon services secret key'
 	},
-	'useHLC'               : {
+	'useHLC'                : {
 		'defaultValue': False,
 		'dataType'    : 'boolean',
 		'description' : 'Enables Hermes Led Control for visual feedback from your assistant'
 	},
-	'activeLanguage'       : {
+	'activeLanguage'        : {
 		'defaultValue': 'en',
 		'dataType'    : 'list',
 		'values'      : ['en', 'fr', 'de', 'it', 'pt'],
 		'description' : 'Project Alice active language'
 	},
-	'activeCountryCode'    : {
+	'activeCountryCode'     : {
 		'defaultValue': 'US',
 		'dataType'    : 'string',
 		'description' : 'Project Alice active country code'
 	},
-	'aliceAutoUpdate'      : {
+	'aliceAutoUpdate'       : {
 		'defaultValue': False,
 		'dataType'    : 'boolean',
 		'description' : 'Whether Alice should auto update, checked every hour'
 	},
-	'skillAutoUpdate': {
+	'skillAutoUpdate'       : {
 		'defaultValue': False,
 		'dataType'    : 'boolean',
 		'description' : 'Whether skills should auto update, checked every 15 minutes'
 	},
-	'githubUsername': {
+	'githubUsername'        : {
 		'defaultValue': '',
 		'dataType'    : 'string',
 		'description' : 'Not mendatory, your github username and token allows you to use Github API much more, such as checking for skills, updating them etc etc'
 	},
-	'githubToken': {
+	'githubToken'           : {
 		'defaultValue': '',
 		'dataType'    : 'password',
 		'description' : 'Not mendatory, your github username and token allows you to use Github API much more, such as checking for skills, updating them etc etc'
 	},
-	'aliceUpdateChannel': {
+	'aliceUpdateChannel'    : {
 		'defaultValue': 'master',
 		'dataType'    : 'list',
 		'values'      : {'Stable': 'master', 'Release candidate': 'rc', 'Beta': 'beta', 'Alpha': 'alpha'},
 		'description' : 'Choose your update frequency. Release is the only supposedly safe option! But if you like to live on the edge, alpha will allow you to preview what\'s coming next!'
 	},
-	'skillsUpdateChannel': {
+	'skillsUpdateChannel'   : {
 		'defaultValue': 'master',
 		'dataType'    : 'list',
 		'values'      : {'Stable': 'master', 'Release candidate': 'rc', 'Beta': 'beta', 'Alpha': 'alpha'},
 		'description' : 'Choose your skill update frequency. Release is the only supposedly safe option! But if you like to live on the edge, alpha will allow you to preview what\'s coming next!',
 		'onUpdate'    : 'refreshStoreData'
 	},
-	'supportedLanguages': {
+	'supportedLanguages'    : {
 		'defaultValue': 'en',
 		'dataType'    : 'list',
 		'values'      : {
@@ -231,50 +250,50 @@ settings = {
 				'countryCode': 'IT'
 			}
 		},
-		'display': 'hidden'
+		'display'     : 'hidden'
 	},
-	'baseCurrency': {
+	'baseCurrency'          : {
 		'defaultValue': 'CHF',
-		'dataType': 'list',
-		'values': ['CHF', 'EUR', 'USD', 'GBP', 'AUD'],
-		'description': 'The currency used by Project Alice'
+		'dataType'    : 'list',
+		'values'      : ['CHF', 'EUR', 'USD', 'GBP', 'AUD'],
+		'description' : 'The currency used by Project Alice'
 	},
-	'baseUnits': {
+	'baseUnits'             : {
 		'defaultValue': 'metric',
-		'dataType': 'list',
-		'values': ['metric', 'kelvin', 'imperial'],
-		'description': 'Units to use with Project Alice'
+		'dataType'    : 'list',
+		'values'      : ['metric', 'kelvin', 'imperial'],
+		'description' : 'Units to use with Project Alice'
 	},
-	'nluEngine': {
+	'nluEngine'             : {
 		'defaultValue': 'snips',
-		'dataType': 'list',
-		'values': ['snips'],
-		'description': 'Natural Language Understanding engine to use'
+		'dataType'    : 'list',
+		'values'      : ['snips'],
+		'description' : 'Natural Language Understanding engine to use'
 	},
-	'onReboot': {
+	'onReboot'              : {
 		'defaultValue': '',
-		'dataType': 'string',
-		'display': 'hidden'
+		'dataType'    : 'string',
+		'display'     : 'hidden'
 	},
-	'webInterfaceActive': {
+	'webInterfaceActive'    : {
 		'defaultValue': True,
-		'dataType': 'boolean',
-		'description': 'Activates the web interface to be reached by browsing to x.x.x.x:webInterfacePort, e.g. 192.168.1.2:5000'
+		'dataType'    : 'boolean',
+		'description' : 'Activates the web interface to be reached by browsing to x.x.x.x:webInterfacePort, e.g. 192.168.1.2:5000'
 	},
-	'webInterfacePort': {
+	'webInterfacePort'      : {
 		'defaultValue': 5000,
-		'dataType': 'integer',
-		'description': 'Change the web interface port to be used'
+		'dataType'    : 'integer',
+		'description' : 'Change the web interface port to be used'
 	},
-	'devMode': {
+	'devMode'               : {
 		'defaultValue': False,
-		'dataType': 'boolean',
-		'description': 'Activates the developer part of the interface, for skill development'
+		'dataType'    : 'boolean',
+		'description' : 'Activates the developer part of the interface, for skill development'
 	},
 
 	# -----------------------
 	# Skills
 	# -----------------------
 
-	'skills': {}
+	'skills'                : {}
 }
