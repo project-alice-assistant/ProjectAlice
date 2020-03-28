318e8aa460445726d864abcd156935d9b4c930b5
Psycho <laurentchervet@bluewin.ch>
2020-03-28 14:07:54 +0100

    This should fix deepspeech

--- core/asr/model/ASR.py
+++ core/asr/model/ASR.py
@@ -3,10 +3,12 @@ from pathlib import Path
 from threading import Event
 from typing import Optional
 
-import pkg_resources
+import re
+from importlib_metadata import PackageNotFoundError, version as packageVersion
 
 from core.asr.model.Recorder import Recorder
 from core.base.model.ProjectAliceObject import ProjectAliceObject
+from core.base.model.Version import Version
 from core.commons import constants
 from core.dialog.model.DialogSession import DialogSession
 
@@ -37,12 +39,37 @@ class ASR(ProjectAliceObject):
 
 	def checkDependencies(self) -> bool:
 		self.logInfo('Checking dependencies')
-		try:
-			pkg_resources.require(self.DEPENDENCIES['pip'])
-			return True
-		except:
-			self.logInfo('Found missing dependencies')
-			return False
+		for dep in self.DEPENDENCIES['pip']:
+			match = re.match(r'^([a-zA-Z-_]*)(?:([=><]{0,2})([\d.]*)$)', dep)
+			if not match:
+				continue
+
+			packageName, operator, version = match.groups()
+			if not packageName:
+				self.logWarning('Wrongly declared PIP requirement')
+				continue
+
+			try:
+				installedVersion = packageVersion(packageName)
+			except PackageNotFoundError:
+				self.logWarning(f'Found missing dependencies: {packageName}')
+				return False
+
+			if not installedVersion or not operator or not version:
+				continue
+
+			version = Version.fromString(version)
+			installedVersion = Version.fromString(installedVersion)
+
+			if (operator == '==' and version != installedVersion) or \
+					(operator == '>=' and installedVersion < version) or \
+					(operator == '>' and (installedVersion < version or installedVersion == version)) or \
+					(operator == '<' and (installedVersion > version or installedVersion == version)):
+
+				self.logWarning(f'Dependency "{packageName}" is not conform with version requirements')
+				return False
+
+		return True
 
 
 	def install(self) -> bool:
