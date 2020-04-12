8d4ebe7b876eac10b897d46d43fe9d4fb2d051f3
Psycho <laurentchervet@bluewin.ch>
2020-04-12 19:46:38 +0200

    I don't understand this shit

--- core/voice/VadManager.py
+++ core/voice/VadManager.py
@@ -1,22 +1,25 @@
-from queue import Queue
+import queue
 
 import struct
 import webrtcvad
 from paho.mqtt.client import MQTTMessage
 
 from core.base.model.Manager import Manager
+from core.commons import constants
 
 
 class VadManager(Manager):
 
 	def __init__(self):
 		super().__init__()
-		self._buffer = Queue()
-		self._vad = webrtcvad.Vad(3)
+		self._buffer = queue.Queue()
+		self._vad = webrtcvad.Vad(2)
+		self._buff = b''
 
 
 	def onBooted(self):
-		#self.MqttManager.mqttClient.subscribe(constants.TOPIC_AUDIO_FRAME.format('office'))
+		return
+		self.MqttManager.mqttClient.subscribe(constants.TOPIC_AUDIO_FRAME.format('office'))
 		self.ThreadManager.newThread(name='vadDetector', target=self.vadDetector)
 
 
@@ -36,19 +39,43 @@ class VadManager(Manager):
 			subChunk2Id, subChunk2Size = struct.unpack('<4sI', message.payload[chunkOffset:chunkOffset + 8])
 			chunkOffset += 8
 			if subChunk2Id == b'data':
-				self._buffer.put(message.payload[chunkOffset:chunkOffset + subChunk2Size])
+				#frame = self.resample(message.payload[chunkOffset:chunkOffset + subChunk2Size])
+				#self._buffer.put(frame)
+				self._buff += message.payload[chunkOffset:chunkOffset + subChunk2Size]
 
 			chunkOffset = chunkOffset + subChunk2Size + 8
 
 
 	def frameGenerator(self):
+		while len(self._buff) < 640:
+			continue
+
 		while True:
-			yield self._buffer.get()
+			chunk = self._buff[:640]
+			self._buff.replace(self._buff[:640], b'')
+			yield chunk
 
 
 	def vadDetector(self):
-		for frame in self.frameGenerator():
-			if len(frame) < 640:
-				return
+		while True:
+			inSpeech = False
+			for frame in self.frameGenerator():
+				now = self._vad.is_speech(frame, 16000)
 
-			print(self._vad.is_speech(frame, 16000))
+				if inSpeech and not now:
+					inSpeech = False
+					self.broadcast(
+						method='vadDown',
+						exceptions=self.name,
+						propagateToSkills=True,
+						siteId='default'
+					)
+				elif not inSpeech and now:
+					inSpeech = True
+					print('speech')
+					self.broadcast(
+						method='vadUp',
+						exceptions=self.name,
+						propagateToSkills=True,
+						siteId='default'
+					)
