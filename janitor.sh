4f91453103dbc27521cfdcc66418dd21d889716e
Psycho <laurentchervet@bluewin.ch>
2020-08-10 06:30:26 +0200

    cleanup

--- janitor.sh
+++ janitor.sh
@@ -6,17 +6,9 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 sudo -u "$(logname)" bash <<EOF
-  cp -r /home/"$(logname)"/ProjectAlice/venv /home/"$(logname)"/venv
   git -C /home/"$(logname)"/ProjectAlice clean -dfx
   git -C /home/"$(logname)"/ProjectAlice checkout master
   git -C /home/"$(logname)"/ProjectAlice pull
-  mv /home/"$(logname)"/venv /home/"$(logname)"/ProjectAlice/venv
-
-  cp -r /home/"$(logname)"/hermesLedControl/venv /home/"$(logname)"/venv
-  git -C /home/"$(logname)"/hermesLedControl clean -dfx
-  git -C /home/"$(logname)"/hermesLedControl checkout master
-  git -C /home/"$(logname)"/hermesLedControl pull
-  mv /home/"$(logname)"/venv /home/"$(logname)"/hermesLedControl/venv
 EOF
 
 apt-get update
@@ -25,13 +17,11 @@ apt-get clean
 apt-get autoclean
 apt-get autoremove -y
 
-rm /boot/ProjectAliceSatellite.yaml
-rm /boot/ProjectAliceSatellite.yaml.bak
-cp /home/"$(logname)"/ProjectAlice/ProjectAliceSatellite.yaml /boot/ProjectAliceSatellite.yaml
+rm /boot/ProjectAlice.yaml
+rm /boot/ProjectAlice.yaml.bak
+cp /home/"$(logname)"/ProjectAlice/ProjectAlice.yaml /boot/ProjectAlice.yaml
 rm /etc/wpa_supplicant/wpa_supplicant.conf
 rm /etc/snips.toml
-cp /home/"$(logname)"/ProjectAlice/system/snips/snips.toml /etc/snips.toml
-rm /etc/systemd/system/hermesledcontrol.service
 systemctl daemon-reload
 systemctl enable ProjectAlice
 
