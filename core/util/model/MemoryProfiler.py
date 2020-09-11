119b7411fd73c98c4285ae80af784518e51ea289
Psycho <laurentchervet@bluewin.ch>
2020-09-11 19:25:07 +0200

    :lipstick: Advanced debug to alice logs

--- core/util/model/MemoryProfiler.py
+++ core/util/model/MemoryProfiler.py
@@ -3,9 +3,10 @@ from pympler import classtracker, muppy, summary, tracker
 from core.base.model.ProjectAliceObject import ProjectAliceObject
 
 
-class MemoryProfiler:
+class MemoryProfiler(ProjectAliceObject):
 
 	def __init__(self):
+		super().__init__()
 		self._tracker = tracker.SummaryTracker()
 		self._aliceTracker = classtracker.ClassTracker()
 		self._aliceTracker.track_class(ProjectAliceObject)
@@ -15,15 +16,17 @@ class MemoryProfiler:
 		allObjects = muppy.get_objects()
 		summ = summary.summarize(allObjects)
 
-		print('----- Objects summary -----')
-		summary.print_(summ)
+		self.logDebug('----- Objects summary -----')
+		for line in summary.format_(summ, limit=15, sort='size', order='descending'):
+			self.logDebug(line)
 
-		print('----- ProjectAliceObject tracker -----')
+		self.logDebug('----- ProjectAliceObject tracker -----')
 		self._aliceTracker.create_snapshot()
 		self._aliceTracker.stats.print_summary()
 
 		if self._tracker:
-			print('----- Evolution over time -----')
-			self._tracker.print_diff()
+			self.logDebug('----- Evolution over time -----')
+			for line in summary.format_(self._tracker.diff()):
+				self.logDebug(line)
 
 		self._tracker = tracker.SummaryTracker()
