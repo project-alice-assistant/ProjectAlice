4f7087eb2ec4a0f400b5fd5e78d86cd09c1fbe58
Psycho <laurentchervet@bluewin.ch>
2020-10-03 05:58:25 +0200

    :bug: fix missing description

--- core/dialog/model/DialogTemplateIntent.py
+++ core/dialog/model/DialogTemplateIntent.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass, field
 @dataclass
 class DialogTemplateIntent:
 	name: str
-	description: str
 	enabledByDefault: bool
 	utterances: list = field(default_factory=list)
 	slots: list = field(default_factory=list)
@@ -17,7 +16,6 @@ class DialogTemplateIntent:
 	def dump(self) -> dict:
 		return {
 			'name'            : self.name,
-			'description'     : self.description,
 			'enabledByDefault': self.enabledByDefault,
 			'utterances'      : self.utterances,
 			'slots'           : self.slots
