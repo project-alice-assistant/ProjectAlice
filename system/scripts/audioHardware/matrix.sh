9ce6a47132530250acf1dc9b60281f220af92300
Psychokiller1888 <laurentchervet@bluewin.ch>
2021-04-14 12:29:21 +0200

    Injecting license

--- system/scripts/audioHardware/matrix.sh
+++ system/scripts/audioHardware/matrix.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+#
+# Copyright (c) 2021
+#
+# This file, matrix.sh, is part of Project Alice.
+#
+# Project Alice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
+#
+# Last modified: 2021.04.13 at 12:56:49 CEST
+#
+
 curl https://apt.matrix.one/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.matrix.one/raspbian $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/matrixlabs.list
 apt-get update
