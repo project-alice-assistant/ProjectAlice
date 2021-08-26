e0c60ab8810df93bfb62549fa82867ea4dd26448
Psychokiller1888 <laurentchervet@bluewin.ch>
2021-08-26 16:59:32 +0200

    Added rebootAfterInstall

--- system/scripts/audioHardware/matrix.sh
+++ system/scripts/audioHardware/matrix.sh
@@ -21,10 +21,10 @@
 # Last modified: 2021.04.13 at 12:56:49 CEST
 #
 
-curl https://apt.matrix.one/doc/apt-key.gpg | sudo apt-key add -
+curl https://s3.amazonaws.com/apt.matrix.one/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.matrix.one/raspbian $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/matrixlabs.list
 apt-get update
-apt-get upgrade -y
+apt-get update -y
 apt-get install -y matrixio-creator-init libmatrixio-creator-hal libmatrixio-creator-hal-dev
 
 sudo -u "$(logname)" bash <<EOF
