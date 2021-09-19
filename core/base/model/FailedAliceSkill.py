934de6cd0d927d62e24130874efa87b4aa843e27
Philipp <41761223+philipp2310@users.noreply.github.com>
2021-09-19 17:48:31 +0200

    add modified status for Failed skills

--- core/base/model/FailedAliceSkill.py
+++ core/base/model/FailedAliceSkill.py
@@ -35,6 +35,7 @@ class FailedAliceSkill(ProjectAliceObject):
 		self._installer = installer
 		self._updateAvailable = False
 		self._name = installer['name']
+		self._modified = False
 		self._icon = self._installer.get('icon', 'fas fa-biohazard')
 		self._aliceMinVersion = Version.fromString(self._installer.get('aliceMinVersion', '1.0.0-b4'))
 		self._maintainers = self._installer.get('maintainers', list())
@@ -79,6 +80,29 @@ class FailedAliceSkill(ProjectAliceObject):
 
 
 	@property
+	def modified(self) -> bool:
+		return self._modified
+
+
+	@modified.setter
+	def modified(self, value: bool):
+		"""
+		As the skill has no writeToDB method and this is the only value that has to be saved right away
+		a update of the value on the DB is performed. This should only occure manually triggered when the user starts to make local changes
+		:param value:
+		:return:
+		"""
+		self._modified = value
+		self.SkillManager.setSkillModified(skillName=self._name, modified=self._modified)
+		dbVal = 1 if value else 0
+		self.logInfo(f'Wrote dbval {dbVal}')
+		self.DatabaseManager.update(tableName=self.SkillManager.DBTAB_SKILLS,
+		                            callerName=self.SkillManager.name,
+		                            row=('skillname', self._name),
+		                            values={'modified': dbVal})
+
+
+	@property
 	def skillPath(self) -> Path:
 		return self._skillPath
 
@@ -89,14 +113,15 @@ class FailedAliceSkill(ProjectAliceObject):
 
 	def toDict(self) -> dict:
 		return {
-			'name'            : self._name,
-			'author'          : self._installer['author'],
-			'version'         : self._installer['version'],
-			'updateAvailable' : self._updateAvailable,
-			'maintainers'     : self._maintainers,
-			'settings'        : self.ConfigManager.getSkillConfigs(self._name),
-			'icon'            : self._icon,
-			'description'     : self._description,
-			'category'        : self._category,
-			'aliceMinVersion' : str(self._aliceMinVersion)
+			'name'           : self._name,
+			'author'         : self._installer['author'],
+			'version'        : self._installer['version'],
+			'modified'       : self._modified,
+			'updateAvailable': self._updateAvailable,
+			'maintainers'    : self._maintainers,
+			'settings'       : self.ConfigManager.getSkillConfigs(self._name),
+			'icon'           : self._icon,
+			'description'    : self._description,
+			'category'       : self._category,
+			'aliceMinVersion': str(self._aliceMinVersion)
 		}
