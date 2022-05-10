b5aba1d055fda69ab376848a540a7f7f015bdb26
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2022-05-11 01:03:59 +0200

    allow dialog Template file without slotTypes when not required

--- core/dialog/DialogTemplateManager.py
+++ core/dialog/DialogTemplateManager.py
@@ -84,6 +84,8 @@ class DialogTemplateManager(Manager):
 
 		for resource in self.skillResource():
 			data = json.loads(resource.read_text())
+			if 'slotTypes' not in data:
+				data['slotTypes'] = []
 			dialogTemplate = DialogTemplate(**data)
 
 			# If we have an utterance extender, apply it
